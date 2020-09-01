5298df1d61d72fcbda8b6f0286309cb33a6b6eab
jeffreyzhu604 <jeffreyzhuu@gmail.com>
2020-08-31 17:08:23 -0700

    client code

---
+++ client       /src/components/Buttons/AddOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddOne = () => {
+    return (
+        <div>
+            <button classname="ui button primary">+1</button>
+        </div>
+    )
+}
+
+export default AddOne;
---
+++ client       /src/components/Buttons/SubtractOne.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SubtractOne = () => {
+    return (
+        <div>
+            <button className="ui button negative">-1</button>
+        </div>
+    )
+}
+
+export default SubtractOne;
